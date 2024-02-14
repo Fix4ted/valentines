85e69bf53c0a094ab70d198daaff6a137ee387af
Fix4ted <160036535+Fix4ted@users.noreply.github.com>
2024-02-14 15:42:57 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+const sr = Scrollreveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+Scrollrevealreveal('baby-text',{delay:200, origin:'top'});
+Scrollreveal('our-image',{delay:450, origin:'top'});
